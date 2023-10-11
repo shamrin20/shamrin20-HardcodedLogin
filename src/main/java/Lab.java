b799d3c0d9d601daa05ad20f3bc3bc4e71aef0c0
shamrin20 <shamrinmillet20@gmail.com>
2023-10-11 13:41:40 +0530

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,13 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        if ("admin".equals(username) && "qwerty".equals(password)) {
+            return true; 
+        } else if ("user".equals(username) && "password".equals(password)) {
+            return true; 
+        } else {
+            return false; 
+        }
     }
 }
+
